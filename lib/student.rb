853ce96c0fff95c5cb8e99ab056e87181c960254
Kristen Peck <kpeck71@gmail.com>
2017-11-24 14:45:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,12 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  def save
+  end
 
+  def create
+  end
+  
   def drop_table
   end
 end
